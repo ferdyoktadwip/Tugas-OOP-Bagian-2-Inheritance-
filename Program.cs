d116cc7918b00137b2bae6d740d2b55832f7f5d5
ferdyoktadwip <ferdy.198@students.amikom.ac.id>
2020-04-09 13:41:53 +0700

    tugas

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LatihanInheritanceReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ferdy", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Bella", 35, 190302, "Guru Ekonomi");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Cindy", 21, 10506, "cindywidyawati@gmail.com");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
